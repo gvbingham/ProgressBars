1d5091a219b404ef39e2926303c548dc5cb98a9b
gbingham <gbingham@bluehost.com>
2015-05-22 15:12:10 -0600

    added the google api to its own file

---
+++ public/lib/goog.js
@@ -0,0 +1,48 @@
+var CLIENT_ID = '791463501485-t6cr3ojjbsdh1vpfj7532u78cb5qqpov.apps.googleusercontent.com';
+var SCOPES = 'https://www.googleapis.com/auth/drive.appfolder';
+
+/**
+* Called when the client library is loaded to start the auth flow.
+*/
+function handleClientLoad() {
+    window.setTimeout(checkAuth, 1);
+}
+
+/**
+* Check if the current user has authorized the application.
+*/
+function checkAuth() {
+    gapi.auth.authorize(
+        {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': true},
+        handleAuthResult
+    );
+}
+
+/**
+* Called when authorization server replies.
+*
+* @param {Object} authResult Authorization result.
+*/
+function handleAuthResult(authResult) {
+    /*var authButton = document.getElementById('authorizeButton');
+    var filePicker = document.getElementById('filePicker');
+    authButton.style.display = 'none';
+    filePicker.style.display = 'none';
+    */
+    if (authResult && !authResult.error) {
+        // Access token has been successfully retrieved, requests can be sent to the API.
+        //filePicker.style.display = 'block';
+        //filePicker.onchange = uploadFile;
+    }
+    else {
+        // No access token could be retrieved, show the button to start the authorization flow.
+        //authButton.style.display = 'block';
+        var item = document.getElementById('header_container');
+        item.onclick = function() {
+            gapi.auth.authorize(
+                {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': false},
+                handleAuthResult
+            );
+        };
+    }
+}
