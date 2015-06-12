afd1c43962ff3b58ab3321f285a3b4ab52b30256
Dillan <dillan.hildebrand@live.com>
2015-06-11 18:18:29 -0600

    updatefile() inserts stringified json now

--- public/lib/goog.js
+++ public/lib/goog.js
@@ -32,26 +32,46 @@ function insertfile () {
     });
     request.execute(function(resp) {
         BAR.settings.app_file_id = resp.id;
-        console.log(resp.id)
     });
 }
 
-function updatefile () {
-    var json = {a:1, b:2};
-    json = JSON.stringify(json);
-    json = btoa(json);
-    var http_text = 'Content-Type: application/json\r\n' +
+function updatefile (json_raw) {
+    json_str = JSON.stringify(json_raw);
+    json_str = btoa(json_str);
+
+    const boundary = '-------314159265358979323846';
+    const delimiter = "\r\n--" + boundary + "\r\n";
+    const close_delim = "\r\n--" + boundary + "--";
+
+    var contentType = 'application/json';
+    var metadata = {
+      'title': 'testzing',
+      'mimeType': contentType
+    };
+
+    var multipartRequestBody =
+        delimiter +
+        'Content-Type: application/json\r\n\r\n' +
+        JSON.stringify(metadata) +
+        delimiter +
+        'Content-Type: ' + contentType + '\r\n' +
         'Content-Transfer-Encoding: base64\r\n' +
         '\r\n' +
-        json;
-    console.log(http_text);
-    var request = gapi.client.drive.files.update({
-        fileId : BAR.settings.app_file_id,
-        uploadType : 'media',
-        body : http_text,
+        json_str +
+        close_delim;
+
+    var request = gapi.client.request({
+        'path': '/upload/drive/v2/files',
+        'method': 'POST',
+        'params': {'uploadType': 'multipart'},
+        'headers': {
+          'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+        },
+        'body': multipartRequestBody
     });
-    request.execute(function(resp) {
-        console.log(resp)
+
+    request.execute(function(data) {
+        console.log(data);
     });
 }
 
