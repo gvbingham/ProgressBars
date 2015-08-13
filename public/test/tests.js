56bb6ffbb63449b2a0d31b732106d7146b5591fa
gbingham <gbingham@bluehost.com>
2015-08-13 16:26:29 -0600

    deleted some node_modules and added a few

--- public/test/tests.js
+++ public/test/tests.js
@@ -10,50 +10,6 @@ suite('time.js', function () {
             assert.match(get_time(), /^\d{13}$/,  'matches the correct digits');
         });
     });
-    suite('get_relative_time_difference', function () {
-        test('is a function', function () {
-            assert.isFunction(get_relative_time_difference, 'get_relative_time_difference is a function'); 
-        });
-        test('returns an object', function () {
-            assert.isObject(get_relative_time_difference(500), 'get_relative_time_difference returns an object');
-        });
-        test('limit test on 0', function () {
-            var obj = { //tobe in data.js file 
-                Days: 0,
-                Hours: 0, 
-                Minutes: 0, 
-                Months: 0, 
-                Seconds: 0, 
-                Weeks: 0, 
-                Years: 0 
-            };
-            assert.deepEqual(get_relative_time_difference(0), obj, 'matches obj with all 0 values'); 
-        });
-        test('limit test on -10', function () {
-            var obj = { //tobe in data.js file 
-                Days: 0,
-                Hours: 0, 
-                Minutes: 0, 
-                Months: 0, 
-                Seconds: 0, 
-                Weeks: 0, 
-                Years: 0 
-            };
-            assert.deepEqual(get_relative_time_difference(-10), obj, 'matches obj with all 0 values if given a negative number'); 
-        });
-        test('check correct count on 34858861', function () {
-            var obj = { //tobe in data.js file 
-                Days: 1,
-                Hours: 1, 
-                Minutes: 1, 
-                Months: 1, 
-                Seconds: 1, 
-                Weeks: 1, 
-                Years: 1 
-            };
-            assert.deepEqual(get_relative_time_difference(34858861), obj, 'matches obj with all 1 values'); 
-        });
-    });
     suite('get_relative_time_text', function () {
         test('is a function', function () {
             assert.isFunction(get_relative_time_text);
